4a5c588c02d79595a79bf1ff6d66ac6df454d248
acristoff <acristoffanini@gmail.com>
2025-02-20 12:28:47 -0800

    signal planning, measure node refinement

--- Game/SheetBuilder/sheet_builder.gd
+++ Game/SheetBuilder/sheet_builder.gd
@@ -3,8 +3,11 @@ class_name SheetBuilder
 
 @export_file("*.json") var music_sheet = "res://Game/Lib/Composer/Music_Sheets/debugsheet.json"
 
-var music
+@onready var name_label = $VBoxContainer3/NameLabel
+@onready var bpm_label = $VBoxContainer3/BPMLabel
+@onready var song_path_label = $VBoxContainer3/SongPathLabel 
 
+var music
 var bpm
 var song_path
 var song_name
@@ -28,9 +31,17 @@ func _ready() -> void:
 func initialize():
 	var json_as_text = FileAccess.get_file_as_string(music_sheet)
 	var sheet_data = JSON.parse_string(json_as_text)
+
 	song_path = sheet_data.song
 	music = load(song_path)
+	song_path_label.text = song_path
+
+	song_name = sheet_data.name
+	name_label.text = song_name
+		
 	bpm = sheet_data.bpm
+	bpm_label.text = str("BPM: ", bpm)
+	
 	sheet = sheet_data.sheet
 	print(sheet_data)
 
@@ -40,4 +51,24 @@ func _on_play_song_button_pressed() -> void:
 
 func _on_stop_button_pressed() -> void:
 	#TODO stop music
+	pass # Replace with function body.
+
+func _on_test_button_pressed() -> void:
+	#TODO go to a debug stage and play this track
+	pass # Replace with function body.
+
+func _on_save_button_pressed() -> void:
+	#TODO Save the file
+	pass # Replace with function body.
+
+func _on_load_button_pressed() -> void:
+	#TODO Load the file
+	pass # Replace with function body.
+
+func _on_edit_button_pressed() -> void:
+	#TODO Bring up a panel that will let you edit the track properties like bpm and name
+	pass # Replace with function body.
+
+func _on_new_sheet_button_pressed() -> void:
+	#TODO New sheet
 	pass # Replace with function body.
