98c92111142df6be1ff805356bc1da22dfab538c
Alex Cristoffanini <acristoffanini@gmail.com>
2025-02-21 21:28:23 -0800

    hold focus until enter

--- Game/SheetBuilder/remap_note_button.gd
+++ Game/SheetBuilder/remap_note_button.gd
@@ -17,7 +17,8 @@ func _ready():
 
 func _toggled(new_button_pressed):
 	set_process_unhandled_input(new_button_pressed)
-	if button_pressed:
+	##TODO FIX THIS FIND ANOTHER WAY YOU SHALL NOT PASS POLISH PHASE
+	if button_pressed && Input.is_action_pressed("ui_accept"):
 		text = "?"
 		#release_focus()
 	else:
@@ -25,29 +26,48 @@ func _toggled(new_button_pressed):
 		#grab_focus()
 
 func _unhandled_input(event):
+	#print(event)
 	if event.is_action_pressed("Blue"):
 		text = "B"
 		send_note("B")
-		button_pressed = false
+		#button_pressed = false
 	if event.is_action_pressed("Red"):
 		text = "R"
 		send_note("R")
-		button_pressed = false
+		#button_pressed = false
 	if event.is_action_pressed("Green"):
 		text = "G"
 		send_note("G")
-		button_pressed = false
+		#button_pressed = false
 	if event.is_action_pressed("Yellow"):
 		text = "Y"
 		send_note("Y")
-		button_pressed = false
+		#button_pressed = false
 	if event.is_action_pressed("ui_cancel"):
 		text = str(num)
 		send_note(num)
 		button_pressed = false
+	if event.is_action("ui_accept"):
+		button_pressed = false
+		call_deferred("lose_focus")
+
+func lose_focus():
+	call_deferred("lose_focus2")
+	button_pressed = false
+
+func lose_focus2():
+	button_pressed = false
+
+func update_note(num):
+	pass
 
 func send_note(note):
-	new_note.emit(note)
+	var note_data = {
+		"note": note,
+		"type": "P",
+		"hold": 0
+	}
+	new_note.emit(note_data)
 
 func update_key_text():
 	#var aux = InputMap.action_get_events(note)[0].as_text()
