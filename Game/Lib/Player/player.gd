c3a172e3e788d48a1501f80c3d1a1e31f0edc94a
Jyunaut <edmartinh@gmail.com>
2025-02-22 03:12:25 -0800

    Added attack states

--- Game/Lib/Player/player.gd
+++ Game/Lib/Player/player.gd
@@ -5,12 +5,33 @@ func _ready():
 
 # NOTE: This is only a test btw
 func _process(delta):
-	if Input.is_action_pressed("ui_up"):
-		state_machine.change_state("WindBrace")
-	elif Input.is_action_just_pressed("ui_down"):
-		state_machine.change_state("Attack_D")
+	if Input.is_action_just_pressed("ui_up"):
+		do_attack_W()
 	elif Input.is_action_just_pressed("ui_left"):
-		state_machine.change_state("Attack_D")
+		do_attack_A()
+	elif Input.is_action_just_pressed("ui_down"):
+		do_attack_S()
 	elif Input.is_action_just_pressed("ui_right"):
-		state_machine.change_state("Attack_D")
+		do_attack_D()
 	pass
+
+func do_idle():
+	state_machine.set_state("Idle")
+	
+func do_flying():
+	state_machine.set_state("Flying")
+
+func do_attack_W():
+	state_machine.set_state("Attack_W")
+	
+func do_attack_A():
+	state_machine.set_state("Attack_A")
+
+func do_attack_S():
+	state_machine.set_state("Attack_S")
+
+func do_attack_D():
+	state_machine.set_state("Attack_D")
+
+func do_hold():
+	state_machine.set_state("Hold")
